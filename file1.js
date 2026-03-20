c0e2903a217b3a885ab6852ff1404e8a49488a6c
lakshmanagupta <46686164+lakshmanagupta@users.noreply.github.com>
2026-03-20 10:15:13 +0530

    Learning node js journey - commit 1

---
+++ file1.js
@@ -0,0 +1,7 @@
+
+
+var a = " Welcome to JavaScript! ";
+var myName = "Lakshman"
+var nodeMsg = "Let's start learning nodeJS"
+var message = "Hello " + myName + " " + nodeMsg;
+console.log(message);
